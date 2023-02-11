3b1d1a8c3e0cd6406134205b6336a9012d74d71e
Hendrypoul <106142503+Hendrypoul@users.noreply.github.com>
2023-02-11 17:30:12 +0530

    11022023

---
+++ script.js
@@ -0,0 +1,30 @@
+let arr = [
+    { id: 1, name: "john", age: "18", marks: 80 },
+    { id: 2, name: "jack", age: "20", marks: 85 },
+    { id: 3, name: "karen", age: "19", marks: 35 },
+  ];
+  
+  function PrintStudentswithMap() {
+    arr.map();
+    console.log(arr);
+  }
+  
+  function PrintStudentsbyForEach() {
+   arr.forEach();
+   console.log(arr);
+  }
+  
+  function addData() {
+   arr.push();
+   console.log(arr);
+  }
+  
+  function removeFailedStudent() {
+    arr.remove();
+    console.log(arr);
+  }
+  
+  function concatenateArray() {
+    arr.concat();
+    console.log(arr);
+  }
